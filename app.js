0b17836bb24620a728c8f2528441feba6510d30b
Hayden <chfrank9011@gmail.com>
2019-04-01 12:40:38 -0500

    added several steps

--- app.js
+++ app.js
@@ -8,3 +8,30 @@ function setName() {
     console.log(name);
 }
 
+console.log('derf');
+let avg = findAvg(2, 2);
+console.log('final derf', avg);
+function findAvg(a, b) {
+    console.log('derf func block');
+    var answerAvg = (a + b) /2;
+    return answerAvg;
+}
+
+let fruits = ['banana', 'tangerine', 'apple'];
+
+
+function showFruits() {
+    let favFruit = fruits[2]
+    console.log(fruits[0]);
+
+    function showFavFruit() {
+        console.log(favFruit);
+    }
+}
+
+showFruits();
+
+
+
+showFavFruit();
+
